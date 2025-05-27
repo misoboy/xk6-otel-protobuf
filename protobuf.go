bdb2c1de57ba1ea8b7bd7fc241a2d713f48d7bd1
misoboy <misoboy.kor@gmail.com>
2025-05-27 10:34:13 +0900

    Modify module

--- protobuf.go
+++ protobuf.go
@@ -1,9 +1,13 @@
-package otel
+package main
 
 import (
 	"github.com/misoboy/xk6-otel-protobuf/otel/encoder"
 	"go.k6.io/k6/js/modules"
 )
+
+func init() {
+	modules.Register("k6/x/protobuf", new(OtelModule))
+}
 
 type OtelModule struct{}
 
