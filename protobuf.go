c28249d6089a9ab352531394a19ea7ce97819eec
misoboy <misoboy.kor@gmail.com>
2025-05-27 11:12:38 +0900

    Modify module

--- protobuf.go
+++ protobuf.go
@@ -1,23 +1,59 @@
 package protobuf
 
 import (
+	"encoding/json"
 	"go.k6.io/k6/js/modules"
+	"google.golang.org/protobuf/proto"
+
+	collogpb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
+	colmetricspb "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
+	coltracepb "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 )
 
 func init() {
-	modules.Register("k6/x/protobuf", new(OtelModule))
+	modules.Register("k6/x/protobuf", new(Protobuf))
 }
 
-type OtelModule struct{}
+type Protobuf struct{}
 
-func (oi *OtelModule) EncodeMetric(jsonStr string) ([]byte, error) {
-	return EncodeMetricFromJSON(jsonStr)
+func (oi *Protobuf) EncodeMetric(jsonStr string) ([]byte, error) {
+	return encodeMetricFromJSON(jsonStr)
 }
 
-func (oi *OtelModule) EncodeTrace(jsonStr string) ([]byte, error) {
-	return EncodeTraceFromJSON(jsonStr)
+func (oi *Protobuf) EncodeTrace(jsonStr string) ([]byte, error) {
+	return encodeTraceFromJSON(jsonStr)
 }
 
-func (oi *OtelModule) EncodeLog(jsonStr string) ([]byte, error) {
-	return EncodeLogFromJSON(jsonStr)
+func (oi *Protobuf) EncodeLog(jsonStr string) ([]byte, error) {
+	return encodeLogFromJSON(jsonStr)
+}
+
+// Metric JSON → protobuf
+func encodeMetricFromJSON(jsonStr string) ([]byte, error) {
+	var req colmetricspb.ExportMetricsServiceRequest
+	err := json.Unmarshal([]byte(jsonStr), &req)
+	if err != nil {
+		return nil, err
+	}
+	return proto.Marshal(&req)
+}
+
+// Trace JSON → protobuf
+func encodeTraceFromJSON(jsonStr string) ([]byte, error) {
+	var req coltracepb.ExportTraceServiceRequest
+	err := json.Unmarshal([]byte(jsonStr), &req)
+	if err != nil {
+		return nil, err
+	}
+	return proto.Marshal(&req)
+}
+
+// Log JSON → protobuf
+func encodeLogFromJSON(jsonStr string) ([]byte, error) {
+	var req collogpb.ExportLogsServiceRequest
+	err := json.Unmarshal([]byte(jsonStr), &req)
+	if err != nil {
+		return nil, err
+	}
+	return proto.Marshal(&req)
 }
